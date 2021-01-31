7f8665316e21fbd1075d14098009be565a748285
apt-get2Update <thirumurugan3131@gmail.com>
2021-01-31 14:47:51 +0530

    model wire

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.Station;
+import com.example.demo.model.Trip;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,20 +18,31 @@ public class HomeController {
                         new Station("CHN","Chennai"),
                         new Station("BLR","Bengaluru"),
                         new Station("MDU","Madurai"),
-                        new Station("CBE","Coimbatore")
+                        new Station("CBE","Coimbatore yy")
                 ));
         return "index";
     }
 
     @RequestMapping("/bus-tickets/from/{fromStation}/to/{toStation}")
     public String busTickets(final Model model) {
-        model.addAttribute("fromStations"
+        model.addAttribute("trips"
                 , List.of(
-                        new Station("CHN","Chennai"),
-                        new Station("BLR","Bengaluru"),
-                        new Station("MDU","Madurai"),
-                        new Station("CBE","Coimbatore")
+                        new Trip(3,"chenai to madurai","340","AcSleeper"),
+        new Trip(5,"chenai to madurai -2","340","AcSleeper")
                 ));
-        return "index";
+        return "trips";
+    }
+    /*
+    * Routring ed point
+    * */
+    @RequestMapping("/bus-tickets/from/{fromStation}/to/{toStation}/sheet-selection")
+    public String sheets(final Model model) {
+       ;
+        //accessing file name of the html
+        return "seat_selection";
+    }
+    @RequestMapping("/passengers-list")
+    public String passangers(final Model model) {
+        return "passengers";
     }
 }
--- src/main/java/com/example/demo/model/Trip.java
+++ src/main/java/com/example/demo/model/Trip.java
@@ -1,4 +1,30 @@
 package com.example.demo.model;
 
 public class Trip {
+    private  Integer avilableSheets;
+    private String rootName;
+    private  String fare;
+    private  String busType;
+    public Trip(Integer avilableSheets, String rootName, String fare, String busType) {
+        this.avilableSheets = avilableSheets;
+        this.rootName = rootName;
+        this.fare = fare;
+        this.busType = busType;
+    }
+
+    public Integer getAvilableSheets() {
+        return avilableSheets;
+    }
+
+    public String getRootName() {
+        return rootName;
+    }
+
+    public String getFare() {
+        return fare;
+    }
+
+    public String getBusType() {
+        return busType;
+    }
 }
