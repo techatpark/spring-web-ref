ca60a980399efb070ca30c8568126f6466875912
sathishk <sathishkumar.thiyagarajan@gmail.com>
2021-02-04 13:25:16 +0530

    tl fix

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -4,17 +4,27 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.io.StringWriter;
 
 @Controller
 public class HomeController {
 
-    @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
+
+    private final TemplateEngine templateEngine;
+
+    public HomeController(TemplateEngine templateEngine) {
+        this.templateEngine = templateEngine;
+    }
+
+    @GetMapping(value = "/"
+            , produces = MediaType.TEXT_HTML_VALUE)
     @ResponseBody
-    public String welcomeAsHTML() {
-        return "<html>\n" + "<header><title>Welcome</title></header>\n" +
-                "<body>\n" + "Hello world\n" + "</body>\n" + "</html>";
+    public String welcomeAsHTML(final Model model) {
+        return templateEngine.process("index",new Context());
     }
 
     @GetMapping("/login")
