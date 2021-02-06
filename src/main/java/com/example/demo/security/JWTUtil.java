3702ceaa75ccd7539548db9ae0f67237e12e4ec7
sathishk <sathishkumar.thiyagarajan@gmail.com>
2021-02-06 13:56:54 +0530

    tken obfu

--- src/main/java/com/example/demo/security/JWTUtil.java
+++ src/main/java/com/example/demo/security/JWTUtil.java
@@ -2,18 +2,26 @@ package com.example.demo.security;
 
 import com.example.demo.model.User;
 import java.security.Key;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.security.MessageDigest;
+import java.util.*;
 
 import javax.annotation.PostConstruct;
 
+import com.example.demo.util.UUIDGenerator;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
 /**
  *
@@ -21,6 +29,9 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class JWTUtil {
+
+	@Autowired
+	private CacheManager cacheManager;
 	
 	@Value("${springbootwebfluxjjwt.jjwt.secret}")
 	private String secret;
@@ -30,13 +41,43 @@ public class JWTUtil {
 
 	private Key key;
 
+	private Cache authCache;
+
+
 	@PostConstruct
 	public void init(){
 		this.key = Keys.hmacShaKeyFor(secret.getBytes());
+		this.authCache = cacheManager.getCache("Auth");
 	}
 
 	public Claims getAllClaimsFromToken(String token) {
 		return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+	}
+
+	public Mono<Authentication> getAuthentication(String digest) {
+		Cache.ValueWrapper valueWrapper = authCache.get(digest);
+		String authToken = (String) valueWrapper.get();
+		if(authToken == null) {
+			return Mono.empty();
+		}
+
+			try {
+				String username = getUsernameFromToken(authToken);
+				if (!validateToken(authToken)) {
+					return Mono.empty();
+				}
+				Claims claims = getAllClaimsFromToken(authToken);
+				List<String> rolesMap = claims.get("role", List.class);
+				List<GrantedAuthority> authorities = new ArrayList<>();
+				for (String rolemap : rolesMap) {
+					authorities.add(new SimpleGrantedAuthority(rolemap));
+				}
+				return Mono.just(new UsernamePasswordAuthenticationToken(username, null, authorities));
+			} catch (Exception e) {
+				return Mono.empty();
+			}
+
+
 	}
 	
 	public String getUsernameFromToken(String token) {
@@ -53,9 +94,15 @@ public class JWTUtil {
 	}
 	
 	public String generateToken(User user) {
+
 		Map<String, Object> claims = new HashMap<>();
 		claims.put("role", user.getRoles());
-		return doGenerateToken(claims, user.getUsername());
+		String jwtToken = doGenerateToken(claims, user.getUsername());
+
+		String digest = UUID.randomUUID().toString();
+
+		authCache.put(digest,jwtToken);
+		return digest;
 	}
 
 	private String doGenerateToken(Map<String, Object> claims, String username) {
